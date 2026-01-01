df2483e5e973745f583a3753fabd2a9db62d9894
arjorb <arjorb@gmail.com>
2026-01-01 09:05:24 +0200

    chore: implement the backend to server the index file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const appName = process.env.APP_NAME
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`${appName} is listening on port ${port}`);
+});
