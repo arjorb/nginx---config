75184c5ea27dd20d465a0de6042e564477c87dc7
arjorb <arjorb@gmail.com>
2026-01-01 09:10:09 +0200

    chore: create a package.json file and add .gitignore file

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const appName = process.env.APP_NAME
-
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`${appName} is listening on port ${port}`);
+    console.log(`node app is listening on port ${port}`);
 });
